3f7487cca72b4c04bb68e7e096880086c20538cb
wararaki <ky7.ott.w@gmail.com>
2020-10-24 22:20:23 +0900

    add template

--- py_sample/sample_django_app/polls/views.py
+++ py_sample/sample_django_app/polls/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.template import loader
 
+from .models import Question
 
-def index(request):
-    return HttpResponse("hello, world!")
+def index(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    response = "You're looking at question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're voting on question {question_id}")
