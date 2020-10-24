6caec22b6aa47c60edd70847388157aba537a4a4
wararaki <ky7.ott.w@gmail.com>
2020-10-24 22:24:16 +0900

    add detail page

--- py_sample/sample_django_app/polls/views.py
+++ py_sample/sample_django_app/polls/views.py
@@ -1,18 +1,21 @@
-from django.http import HttpResponse, HttpRequest
-from django.template import loader
+from django.http import HttpResponse, HttpRequest, Http404
+from django.shortcuts import render
 
 from .models import Question
 
 def index(request: HttpRequest) -> HttpResponse:
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exists')
+    return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request: HttpRequest, question_id: int) -> HttpResponse:
     response = "You're looking at question %s"
