a9050b0e2c71cede0a3a07cd992f94ac31fd1f7c
wararaki <ky7.ott.w@gmail.com>
2020-10-24 22:26:27 +0900

    update

--- py_sample/sample_django_app/polls/views.py
+++ py_sample/sample_django_app/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, HttpRequest, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -11,10 +11,7 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'polls/index.html', context)
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exists')
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request: HttpRequest, question_id: int) -> HttpResponse:
