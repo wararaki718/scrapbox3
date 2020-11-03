69be9a4942bd5acae80927b22c56a522bc2606d6
wararaki <ky7.ott.w@gmail.com>
2020-11-03 21:22:01 +0900

    add user keys

--- py_sample/django_rest_tutorial/snippets/serializers.py
+++ py_sample/django_rest_tutorial/snippets/serializers.py
@@ -1,9 +1,20 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
-    
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
