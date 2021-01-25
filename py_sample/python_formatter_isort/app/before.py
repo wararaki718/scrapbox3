405d224367afe6f0494f6906b1ae4022c2caa6a0
wararaki <ky7.ott.w@gmail.com>
2021-01-26 00:17:19 +0900

    update

---
+++ py_sample/python_formatter_isort/app/before.py
@@ -0,0 +1,18 @@
+import sys
+
+import os
+
+from module import sample1
+
+import sys
+
+from module import sample2
+
+def main():
+    from module import sample1
+    import sys
+    print('DONE')
+
+
+if __name__ == '__main__':
+    main()
