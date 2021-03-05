98bcf652b57f125f4369413490e2aa113a9e3284
wararaki <ky7.ott.w@gmail.com>
2021-03-06 01:02:51 +0900

    update

--- py_sample/fastapi_websocket/server/main.py
+++ py_sample/fastapi_websocket/server/main.py
@@ -18,5 +18,5 @@ async def get():
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.recieve_text()
+        data = await websocket.receive_text()
         await websocket.send_text(f"Message text was: {data}")
