3f4ef1aa1afb218a970d581e092619f0ef141f64
wararaki <ky7.ott.w@gmail.com>
2021-04-10 19:22:12 +0900

    update

--- kt_sample/blog/src/main/kotlin/com/example/blog/HttpControllers.kt
+++ kt_sample/blog/src/main/kotlin/com/example/blog/HttpControllers.kt
@@ -13,7 +13,7 @@ class ArticleController(private val repository: ArticleRepository) {
     @GetMapping("/")
     fun findAll() = repository.findAllByOrderByAddedAtDesc()
 
-    @GetMapping("/{slug")
+    @GetMapping("/{slug}s")
     fun findOne(@PathVariable slug: String) = repository.findBySlug(slug) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
 }
 
