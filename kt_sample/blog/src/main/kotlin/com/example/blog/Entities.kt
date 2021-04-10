510c63cd79c296ae881199eadf497c45c2b19f64
wararaki <ky7.ott.w@gmail.com>
2021-04-10 18:03:27 +0900

    update

---
+++ kt_sample/blog/src/main/kotlin/com/example/blog/Entities.kt
@@ -0,0 +1,25 @@
+package com.example.blog
+
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.ManyToOne
+import javax.persistence.Id
+
+@Entity
+class Article(
+        var title: String,
+        var headline: String,
+        var content: String,
+        @ManyToOne var author: User,
+        var slug: String = title.toSlug(),
+        var addedAt: LocalDateTime = LocalDateTime.now(),
+        @Id @GeneratedValue var id: Long? = null)
+
+@Entity
+class User(
+        var login: String,
+        var firstname: String,
+        var lastname: String,
+        var description: String? = null,
+        @Id @GeneratedValue var id: Long? = null)
